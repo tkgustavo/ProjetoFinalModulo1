21e908e5c3806e884f2c63ae0ae11999f9aa5eb4
LuizOliveirasp <profluizoliveiranovotec@gmail.com>
2024-08-16 12:46:30 -0300

    refatoraçaõ da classe Funcoes e add validação de email no método cadastrarContato

--- src/br/com/agenda/funcoes/Funcoes.java
+++ src/br/com/agenda/funcoes/Funcoes.java
@@ -1,8 +1,14 @@
 package br.com.agenda.funcoes;
 
 public class Funcoes {
-    public void cadastrarContato() {
-        System.out.println("Contato cadastrado com sucesso!");
+    public String cadastrarContato(String email) {
+
+            if (email == null || !email.contains("@") || email.indexOf('@') == 0 || email.indexOf('@') == email.length() - 1) {
+                return "--------------------------------------------------\n" +
+                        "Email inválido. Por favor, digite um email válido.\n" +
+                        "--------------------------------------------------\n";
+            }
+            return "Contato cadastrado com sucesso!";
     }
     public void listarContatos() {
         System.out.println("Contatos listados com sucesso!");
@@ -14,32 +20,11 @@ public class Funcoes {
         System.out.println("Contato editado com sucesso!");
     }
     public void excluirContato() {
-
         System.out.println("Contato excluído com sucesso!");
     }
-
-    /*
-    Acho que vai precisar da printAgenda também
-     */
     public void imprimirAgenda() {
         System.out.println("Agenda vazia!");
         System.out.println("Exemplo!");
-    }
-
-    //Método para verificar se o email é válido ou não//
-    //Explicando: Se o símbolo do @ existe e não está nem no começo, nem no final, então é válido.
-    //é a forma mais simples que consegui pensar.
-    //para chamar é só digitar verificadorEmail(digitar uma String email)
-
-    public static String verificadorEmail(String email) {
-        int verificador = email.indexOf("@");
-        String mensagem = "";
-        if (verificador <= 0 || verificador == email.length() - 1) {
-            mensagem = "--------------------------------------------------" +
-                    "\nEmail inválido. Por favor, digite um email válido. " +
-                    "\n--------------------------------------------------\n";
-        }
-        return mensagem;
     }
 
 }
--- src/br/com/agenda/main/Main.java
+++ src/br/com/agenda/main/Main.java
@@ -1,9 +1,16 @@
 package br.com.agenda.main;
+import br.com.agenda.funcoes.Funcoes;
 import br.com.agenda.menu.Menu;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        //----------------------Teste da validação simples de email -------------
+
+        Funcoes funcoes = new Funcoes();
+        System.out.println(funcoes.cadastrarContato("outrogmail.com"));
+
 
     }
 
