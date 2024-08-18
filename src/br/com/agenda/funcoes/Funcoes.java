338b90833254c6888a1f2d747d7a01ebe265553a
tkgustavo <gustavohenrique.miranda@hotmail.com>
2024-08-18 15:14:57 -0300

    Refactor nas funçoes e no Menu

--- src/br/com/agenda/funcoes/Funcoes.java
+++ src/br/com/agenda/funcoes/Funcoes.java
@@ -1,239 +1,174 @@
 package br.com.agenda.funcoes;
+
 import br.com.agenda.read.ReadData;
-import br.com.agenda.validacoes.Validacoes;
 
 public class Funcoes {
+
     static int NUMERO_LINHAS = 100;
     static final int NUMERO_COLUNAS = 3;
     static final int IDLENGTH = 4;
     static final int NAMELENGTH = ReadData.NAMELENGTH;
     static final int PHONELENGTH = 11;
-    static final String ID  = "ID";
-    static final String NAME  = "NOME";
-    static final String PHONE  = "TELEFONE";
-    static final String MAIL  = "E-MAIL";
-    // Código ANSI para negrito
-    static final String negrito = "\033[1m";
-    static final String reset = "\033[0m";
-    //Mandar para uma classe de contatos
+    static final String ID = "ID";
+    static final String NAME = "NOME";
+    static final String PHONE = "TELEFONE";
+    static final String MAIL = "E-MAIL";
+
+    static final String NEGRITO = "\033[1m";
+    static final String RESET = "\033[0m";
+
     static String[][] contatos = new String[NUMERO_LINHAS][NUMERO_COLUNAS];
-    // Bloco estático para inicializar a matriz de contatos com strings vazias
+    static int espacoContatos = 0;
+
     static {
-        for (int i = 0; i < contatos.length; i++) {
-            for (int j = 0; j < contatos[i].length; j++) {
-                contatos[i][j] = "";
+        for (String[] contato : contatos) {
+            for (int j = 0; j < contato.length; j++) {
+                contato[j] = "";
             }
         }
-    }
-    static int espacoContatos=0;
-    /*
-    Adiciona contatos
-     */
-    public static void cadastrarContato() {
-        //não testei essa lógica
-        if(espacoContatos == NUMERO_LINHAS) {
-            String name = ReadData.readName();
-            //retorna ao menu caso o usuário tenha saído antes de cadastrar
-            if (name.equals("")) {
-                return;
-            }
-            String phone = ReadData.readPhone();
-            //aqui pode ser melhor inverter a ordem para não ter que limpar o nome caso o usuário não insira o nome;
-            if (phone.equals("")) {
-                return;
-            }
-            String email = ReadData.readMail();
-            espacoContatos++;
-            NUMERO_LINHAS = NUMERO_LINHAS*2;
-            String[][] aux = new String[NUMERO_LINHAS][NUMERO_COLUNAS];
-            for (int i = 0; i < contatos.length; i++){
-                for (int j = 0; j < contatos[i].length; j++) {
-                    aux[i][j] = contatos[i][j];
-                }
-            }
-            contatos = aux;
-            contatos[NUMERO_LINHAS/2][0] = name;
-            contatos[NUMERO_LINHAS/2][1] = phone;
-            contatos[NUMERO_LINHAS/2][2] = email;
-            return;
-        }
-        int posicaoVazia = 0;
-        if (contatos != null) {
-            for (int i = 0; i < contatos.length; i++) {
-                if (contatos[i][1].isEmpty()) {
-                    posicaoVazia = i;
-                    break;
-                }
-            }
-        }
-        contatos[posicaoVazia][0] = ReadData.readName();
-        //retorna ao menu caso o usuário tenha saído antes de cadastrar
-        if (contatos[posicaoVazia][0].equals("")) {
-            return;
-        }
-        contatos[posicaoVazia][1] = ReadData.readPhone();
-        //aqui pode ser melhor inverter a ordem para não ter que limpar o nome caso o usuário não insira o nome;
-        if (contatos[posicaoVazia][1].equals("")) {
-            contatos[posicaoVazia][0] = "";
-            return;
-        }
-        contatos[posicaoVazia][2] = ReadData.readMail();
-        espacoContatos++;
-        System.out.println("Contato cadastrado com sucesso!");
-        return;
     }
 
-    /*
-    Lista contatos
-     */
+    public static void cadastrarContato() {
+        if (espacoContatos == NUMERO_LINHAS) {
+            expandirMatrizContatos();
+        }
+
+        int posicaoVazia = encontrarPosicaoVazia();
+
+        String nome = ReadData.readName();
+        if (nome.isEmpty()) return;
+
+        String telefone = ReadData.readPhone();
+        if (telefone.isEmpty()) return;
+
+        String email = ReadData.readMail();
+
+        contatos[posicaoVazia][0] = nome;
+        contatos[posicaoVazia][1] = telefone;
+        contatos[posicaoVazia][2] = email;
+
+        espacoContatos++;
+        System.out.println("Contato cadastrado com sucesso!");
+    }
+
     public static void listarContatos() {
-        System.out.println("");
-        System.out.println("##############################################################################################");
-        System.out.println("###########################################"+negrito+"CONTATOS"+reset+"###########################################");
-        System.out.println("##############################################################################################");
+        exibirCabecalho("CONTATOS");
 
         System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
         int id = 1;
-        for (int i = 0; i < contatos.length; i++) {
-            if (!contatos[i][1].isEmpty()) {
-                System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", id, contatos[i][0], contatos[i][1], contatos[i][2]);
+        for (String[] contato : contatos) {
+            if (!contato[1].isEmpty()) {
+                System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", id, contato[0], contato[1], contato[2]);
                 id++;
             }
         }
     }
 
-    /*
-    Busca contatos
-     */
     public static void buscarContato() {
         String numeroContato = ReadData.readPhone();
-        //retorna ao menu caso o usuário tenha saído antes de informar um número
-        if (numeroContato.equals("")) {
-            return;
-        }
-        int posicaoContato = 0;
-        for (int i = 0; i < contatos.length; i++) {
-            if (numeroContato.equals(contatos[i][1])) {
-                posicaoContato = i+1;
-                break;
-            }
-        }
-        if (posicaoContato == 0) {
+        if (numeroContato.isEmpty()) return;
+
+        int posicaoContato = encontrarContatoPorNumero(numeroContato);
+
+        if (posicaoContato == -1) {
             System.out.println("Contato não encontrado!");
-            return;
         } else {
-            System.out.println("Contato encontrado com sucesso!");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH+ "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
-            System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", posicaoContato, contatos[posicaoContato-1][0], contatos[posicaoContato-1][1], contatos[posicaoContato-1][2]);
+            exibirContato(posicaoContato + 1);
         }
     }
 
-    /*
-    Edita contatos
-     */
     public static void editarContato() {
         String numeroContato = ReadData.readPhone();
-        //retorna ao menu caso o usuário tenha saído antes de informar um número
-        if (numeroContato.equals("")) {
-            return;
-        }
-        int posicaoContato = 0;
-        for(int i = 0; i<contatos.length;i++) {
-            if (numeroContato.equals(contatos[i][1])) {
-                posicaoContato = i+1;
-                break;
-            }
-        }
-        if(posicaoContato ==0) {
+        if (numeroContato.isEmpty()) return;
+
+        int posicaoContato = encontrarContatoPorNumero(numeroContato);
+
+        if (posicaoContato == -1) {
             System.out.println("Contato não encontrado!");
-            return;
-        }else {
-            contatos[posicaoContato-1][0] = ReadData.readName();
-            contatos[posicaoContato-1][1] = ReadData.readPhone();
-            contatos[posicaoContato-1][2] = ReadData.readMail();
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
-            System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", posicaoContato, contatos[posicaoContato-1][0], contatos[posicaoContato-1][1], contatos[posicaoContato-1][2]);
+        } else {
+            contatos[posicaoContato][0] = ReadData.readName();
+            contatos[posicaoContato][1] = ReadData.readPhone();
+            contatos[posicaoContato][2] = ReadData.readMail();
+            exibirContato(posicaoContato + 1);
+            System.out.println("Contato atualizado com sucesso!");
+        }
+    }
+
+    public static void excluirContato() {
+        String numeroContato = ReadData.readPhone();
+        if (numeroContato.isEmpty()) return;
+
+        int posicaoContato = encontrarContatoPorNumero(numeroContato);
+
+        if (posicaoContato == -1) {
+            System.out.println("Contato não encontrado!");
+        } else {
+            contatos[posicaoContato][0] = "";
+            contatos[posicaoContato][1] = "";
+            contatos[posicaoContato][2] = "";
+            espacoContatos--;
+
+            if (espacoContatos <= NUMERO_LINHAS / 2 - 10) {
+                reduzirMatrizContatos();
+            }
+
             System.out.println("Contato excluído com sucesso!");
         }
     }
 
-    /*
-    Exclui contato
-     */
-    public static void excluirContato() {
-        String numeroContato = ReadData.readPhone();
-        //retorna ao menu caso o usuário tenha saído antes de informar um número
-        if (numeroContato.equals("")) {
-            return;
-        }
-        int posicaoContato = 0;
-        for(int i = 0; i<contatos.length;i++) {
-            if (numeroContato.equals(contatos[i][1])) {
-                posicaoContato = i+1;
-                break;
-            }
-        }
-        if(posicaoContato ==0) {
-            System.out.println("Contato não encontrado!");
-            return;
-        }else {
-            contatos[posicaoContato-1][0] = "";
-            contatos[posicaoContato-1][1] = "";
-            contatos[posicaoContato-1][2] = "";
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
-            System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", posicaoContato, contatos[posicaoContato-1][0], contatos[posicaoContato-1][1], contatos[posicaoContato-1][2]);
-            espacoContatos--;
-            //não testei essa lógica, retirar o -10 para testar com uns 3 contatos
-            if(espacoContatos == ((NUMERO_LINHAS/2)-10)) {//só reduz a matriz se ela tiver mais de 10 espacos vagos
-                NUMERO_LINHAS = NUMERO_LINHAS/2;
-                String[][] aux = new String[NUMERO_LINHAS][NUMERO_COLUNAS];
-                int posicaoAux = 0;
-                for (int i = 0; i < contatos.length; i++){
-                    if(!contatos[i][1].isEmpty()){
-                        aux[posicaoAux][0] = contatos[i][0];
-                        aux[posicaoAux][1] = contatos[i][1];
-                        aux[posicaoAux][2] = contatos[i][2];
-                        posicaoAux++;
-                    }
-                }
-                contatos = aux;
-            }
-        }
-        System.out.println("Contato excluído com sucesso!");
-        return;
-}
-    /*
-    Acho que vai precisar da printAgenda também
-     */
-    public static void imprimirAgenda() {
-        if (espacoContatos == 0) {
-            System.out.println("");
-            System.out.println("##############################################################################################");
-            System.out.println("############################################"+negrito+"AGENDA"+reset+"############################################");
-            System.out.println("##############################################################################################");
-            System.out.println("###########################################"+negrito+"EXEMPLOS"+reset+"###########################################");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "1", "Diogenes Viana", "11111111111", "diogenes.carvalho.viana@gmail.com");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "2", "Fernando Diniz", "22222222222", "diniz.rocha82@gmail.com");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "3", "Gustavo Miranda", "33333333333", "gustavohenrique.miranda@hotmail.com");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "4", "Kevin Silva", "44444444444", "kevin.silva2010.ks@gmail.com");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "5", "Luiz Oliveira", "55555555555", "luizoliveirautonomo@gmail.com");
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", "5", "Mário Duarte", "66666666666", "mcbd.eng@gmail.com");
-        } /*else {
-            System.out.println("");
-            System.out.println("##############################################################################################");
-            System.out.println("############################################"+negrito+"AGENDA"+reset+"############################################");
-            System.out.println("##############################################################################################");
+    private static void expandirMatrizContatos() {
+        NUMERO_LINHAS *= 2;
+        String[][] aux = new String[NUMERO_LINHAS][NUMERO_COLUNAS];
 
-            System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
-            int id = 1;
-            for (int i = 0; i < contatos.length; i++) {
-                if (!contatos[i][1].isEmpty()) {
-                    System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", id, contatos[i][0], contatos[i][1], contatos[i][2]);
-                    id++;
-                }
+        for (int i = 0; i < contatos.length; i++) {
+            System.arraycopy(contatos[i], 0, aux[i], 0, contatos[i].length);
+        }
+
+        contatos = aux;
+    }
+
+    private static void reduzirMatrizContatos() {
+        NUMERO_LINHAS /= 2;
+        String[][] aux = new String[NUMERO_LINHAS][NUMERO_COLUNAS];
+        int posicaoAux = 0;
+
+        for (String[] contato : contatos) {
+            if (!contato[1].isEmpty()) {
+                aux[posicaoAux] = contato;
+                posicaoAux++;
             }
-        }*/
+        }
+
+        contatos = aux;
+    }
+
+    private static int encontrarPosicaoVazia() {
+        for (int i = 0; i < contatos.length; i++) {
+            if (contatos[i][1].isEmpty()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int encontrarContatoPorNumero(String numeroContato) {
+        for (int i = 0; i < contatos.length; i++) {
+            if (numeroContato.equals(contatos[i][1])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static void exibirCabecalho(String titulo) {
+        System.out.println("");
+        System.out.println("##############################################################################################");
+        System.out.println("############################################" + NEGRITO + titulo + RESET + "############################################");
+        System.out.println("##############################################################################################");
+    }
+
+    private static void exibirContato(int id) {
+        System.out.printf("%-" + IDLENGTH + "s\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", ID, NAME, PHONE, MAIL);
+        System.out.printf("%-" + IDLENGTH + "d\t|\t%-" + NAMELENGTH + "s\t|\t%-" + PHONELENGTH + "s\t|\t%s\n", id, contatos[id - 1][0], contatos[id - 1][1], contatos[id - 1][2]);
     }
 }
-
-
--- src/br/com/agenda/menu/Menu.java
+++ src/br/com/agenda/menu/Menu.java
@@ -1,16 +1,15 @@
 package br.com.agenda.menu;
+
 import br.com.agenda.Enum.FuncoesMenu;
 import br.com.agenda.funcoes.Funcoes;
 import br.com.agenda.read.ReadData;
 
 public class Menu {
-    /*
-    Essa função printa a tela inicial da agenda
-    INTERAR O ENUN
-     */
+
+    // Exibe a tela inicial do menu da agenda
     public static void inicializador() {
         System.out.println("");
-        System.out.println(">>>>>>Menu da Agenda<<<<<<");
+        System.out.println(">>>>>> Menu da Agenda <<<<<<");
         System.out.println("1 - Adicionar Contato");
         System.out.println("2 - Listar Contatos");
         System.out.println("3 - Buscar Contato");
@@ -18,12 +17,12 @@ public class Menu {
         System.out.println("5 - Remover Contato");
         System.out.println("6 - Sair");
     }
+
     public static void menu() {
         FuncoesMenu escolha = null;
         do {
-            Funcoes.imprimirAgenda();
             inicializador();
-            //Cases
+            // Cases
             switch (ReadData.readMenu()) {
                 case 1:
                     Funcoes.cadastrarContato();
@@ -45,12 +44,11 @@ public class Menu {
                     System.out.println("Saindo...");
                     break;
                 default:
-                    System.out.println("Opção inválida! Tente novamente");
+                    System.out.println("Opção inválida! Tente novamente.");
             }
         } while (escolha != FuncoesMenu.SAIR);
-        //Feacha o scanner da classe ReadData
+
+        // Fecha o scanner da classe ReadData
         ReadData.closeScanner();
     }
 }
-
-
